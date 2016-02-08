d836e1122b0a935a8022655999f0917e294544b7
JGluhov <jgluhov@gmail.com>
2016-02-08 16:45:47 +0300

    add stylus support and css

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 /**
  * Created by jgluhov on 08/02/16.
  */
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../styles/main.styl';
 
 console.log(`Hellosss`);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@
  * Created by jgluhov on 08/02/16.
  */
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin'),
+	ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 	context: __dirname + '/src',
@@ -11,7 +12,7 @@ module.exports = {
 	},
 	output: {
 		path: __dirname + '/public',
-		filename: 'js/bundle.js'
+		filename: './js/bundle.js'
 	},
 	module: {
 		loaders: [
@@ -24,12 +25,28 @@ module.exports = {
 				test: /\.jade$/,
 				exclude: /node_modules/,
 				loader: 'jade'
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract('style','css')
+			},
+			{
+				test: /\.styl$/,
+				exclude: /node_modules/,
+				loader: ExtractTextPlugin.extract('style','css!stylus')
+			},
+			{
+				test: /\.(ttf|woff|woff2|eot|svg)$/,
+				loader: 'file?name=./fonts/[name].[ext]'
 			}
 		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './templates/index.jade'
+		}),
+		new ExtractTextPlugin("./css/styles.css", {
+			disable: process.env.NODE_ENV == 'development'
 		})
 	]
 };
